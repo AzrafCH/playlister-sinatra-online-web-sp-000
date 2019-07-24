c55dc4f6cd7ddd6636efdf7bb9700a534fc5cbd4
Azraf Chowdhury <achowdhur@ltu.edu>
2019-07-24 01:28:37 +0000

    Automatically backed up by Learn

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,3 +1,14 @@
 class Slugifiable
+  module ClassMethods
+    def find_by_slug(slug)
+      self.all.find { |artist| artist.slug == slug}
+    end
+  end
+
+  module InstanceMethods
+    def slug
+      self.name.gsub(" ", "-").downcase
+    end
+  end
 
 end
