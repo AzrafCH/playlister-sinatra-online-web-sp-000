5d3458c17282d3c2742a87f7780e6976e493f3c8
Azraf Chowdhury <achowdhur@ltu.edu>
2019-07-24 01:35:32 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,6 @@
 class Genre < ActiveRecord::Base
-  extend Slugifiable::ClassMethods
-  include Slugifiable::InstanceMethods
+  #extend Slugifiable::ClassMethods
+  #include Slugifiable::InstanceMethods
 
   has_many :song_genres
   has_many :songs, through: :song_genres
