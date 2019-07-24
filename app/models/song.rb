d92a282f221ed7ff9af0148c54cb7dd8cd957231
Azraf Chowdhury <achowdhur@ltu.edu>
2019-07-24 01:32:13 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,6 @@ class Song < ActiveRecord::Base
   extend Slugifiable::ClassMethods
   include Slugifiable::InstanceMethods
 
-  belongs_to :artists 
+  belongs_to :artists
   has_many :genres, through: :song_genres
 end
